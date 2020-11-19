4cfb30dc9a0f0fe9c8d85f8dc16f6a2df7b5aad6
yasmeenattallah <yattallah7@gmail.com>
2020-11-19 17:29:06 +0200

    serving search

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,14 @@
-// const searchBtn = document.querySelector('.search-btn');
-// const frontPage = document.querySelector('.front');
-// const resultPage = document.querySelector('.result');
-// searchBtn.addEventListener('click', () => {
-//   frontPage.style.display = 'none';
-//   resultPage.style.display = 'block';
-// });
+const input = document.querySelector('.search-bar');
+const suggestion = document.querySelector('.search-suggestion');
+
+input.addEventListener('input', () => {
+  const val = input.value.trim();
+  fetch('/name', (dataObj) => {
+    const countryName = dataObj.name;
+    countryName.forEach((item) => {
+      if (val === item) {
+        suggestion.appendChild(item);
+      }
+    });
+  });
+});
